6c0051cc136c9691b23bc96256c611b587f9ecd6
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-03-22 19:56:13 +0600

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:travel/Description/deccriptionmain_page.dart';
 import 'package:travel/Description/icon_page.dart';
-import 'package:travel/clippath_page.dart';
-import 'package:travel/datastorhive/hive_page.dart';
 import 'package:travel/home/appsicon_page.dart';
 import 'package:travel/home/home_page.dart';
 import 'package:travel/moment/ravel_page.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hive/hive.dart';
-import 'package:travel/sqflite/barchat_page.dart';
 
-Future <void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
 
-  Box box = await Hive.openBox("country-list");
-  print(box.get("name"));
+void main() {
   runApp(const MyApp());
 }
 
@@ -29,7 +20,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
--- lib/sqflite/contruct.dart
+++
@@ -1,6 +0,0 @@
-class Contruct{
-  int? id;
-  String name;
-  String phone;
-  Contruct({this.id,required this.name,required this.phone});
-}
